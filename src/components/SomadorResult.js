0dceedb05ad8792a09f54816033e17241c7c8a78
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2020-05-27 18:00:21 -0300

    Visor finalizado

--- src/components/SomadorResult.js
+++ src/components/SomadorResult.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 
 const SomadorResult = ({ values }) => {
     let total = 0;
     values.map(v => total += +v.value);
     return (
-        <View>
-            <Text>{total}</Text>
+        <View style={styles.container}>
+            <Text style={styles.result}>{total}</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 15,
+        marginVertical: 30,
+        alignItems: 'center',
+        backgroundColor: '#E5E9A3',
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: '#E5E9A3'
+    },
+    result: {
+        fontSize: 30
+    }
+});
 
 const mapStateToProps = state => {
     const { values } = state;
